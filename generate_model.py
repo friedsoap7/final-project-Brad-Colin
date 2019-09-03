2880b0ce52a8b686ae234833d22971a49dd1bcc6
Colin Lemarchand <clemarch@ucsd.edu>
2019-09-03 10:27:54 -0700

    Added semeion data to training

--- generate_model.py
+++ generate_model.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from main import load_image
 import os
+from skimage.transform import resize
 
 mnist = tf.keras.datasets.mnist
 (training_data, training_labels), (testing_data, testing_labels) = mnist.load_data()
@@ -21,6 +22,63 @@ for i in range(10):
 training_data = np.append(training_data, np.array(extra_data), axis = 0)
 training_labels = np.append(training_labels, extra_labels)
 
+# Open the semeion data file and prepare lists for input
+even_more_data = open('test-data/semeion.data')
+data_string = even_more_data.read()
+data_string = data_string.split()
+extra_data = []
+temp_image = []
+extra_labels = []
+
+# Process the data in the semeion data file
+index_in_line = 0
+for num in data_string:
+    if index_in_line < 256: # First 256 characters on line are pixels
+        temp_image.append(num)
+        index_in_line += 1
+    elif index_in_line == 256: # Loading pixels complete; add image to queue
+        extra_data.append(temp_image)
+        temp_image = []
+        index_in_line += 1
+        if int(float(num)) == 1: # If the first marker is 1, then the label is 0
+            extra_labels.append(0)
+    elif index_in_line < 265: # Characters from indexes 256-265 are reserved for the image's label
+        if int(float(num)) == 1: # If the current marker is 1, then the label is the index (with respect to markers)
+            extra_labels.append(index_in_line - 256)
+        index_in_line += 1
+    else:
+        if int(float(num)) == 1: # If the final marker is 1, then the label is 9
+            extra_labels.append(9)
+        index_in_line = 0
+new_extra_data = []
+for image in extra_data:
+    x = 0
+    y = 0
+    image_list = []
+    temp_list = []
+    for pixel in image:
+        if x == 16:
+            x = 0
+            y += 1
+            image_list.append(np.array(temp_list))
+            temp_list = []
+        temp_list.append(255*int(float(pixel)))
+        x += 1
+    image_list.append(np.array(temp_list))
+    image_list = np.array(image_list)
+    image_list = resize(image_list, (20, 20), anti_aliasing=True)
+    #add padding
+    new_image_list = [[0 for x in range(28)] for y in range(28)]
+    for x in range(20):
+        for y in range(20):
+            new_image_list[4 + y][4 + x] = image_list[y][x]
+    new_image_list = np.array(new_image_list)
+    new_extra_data.append(new_image_list)
+    image_list = []
+new_extra_data = np.array(new_extra_data)
+training_data = np.append(training_data, new_extra_data, axis = 0)
+training_labels = np.append(training_labels, extra_labels)
+
 training_data = tf.keras.utils.normalize(training_data, axis=1).reshape(training_data.shape[0], -1)
 testing_data = tf.keras.utils.normalize(testing_data, axis=1).reshape(testing_data.shape[0], -1)
 
@@ -30,7 +88,7 @@ model.add(tf.keras.layers.Dense(256, activation = tf.nn.relu))
 model.add(tf.keras.layers.Dense(256, activation = tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax))
 
-model.compile(optimizer = 'nadam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
 model.fit(training_data, training_labels, epochs = 3)
 
